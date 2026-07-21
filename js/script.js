a0e875f16e39af384cd8d774844a8b1f5b681ae0
stankov1337-ai <stankov1337@gmail.com>
2026-07-21 14:51:34 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+const contactBtn = document.getElementById("contactBtn");
 
+const contactWindow = document.getElementById("contactWindow");
+
+const overlay = document.getElementById("overlay");
+
+const closeBtn = document.getElementById("closeBtn");
+
+contactBtn.onclick = () => {
+
+    contactWindow.style.display = "block";
+
+    overlay.style.display = "block";
+
+}
+
+closeBtn.onclick = closeWindow;
+
+overlay.onclick = closeWindow;
+
+function closeWindow(){
+
+    contactWindow.style.display = "none";
+
+    overlay.style.display = "none";
+
+}
